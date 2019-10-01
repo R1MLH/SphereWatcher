4c897731a3e335ede79f222a46028930897062e8
Erwan <erwan.mailharro@edu.esiee.fr>
2019-10-01 13:45:44 +0200

    💡💡💡

---
+++ Projet_IMA/Lumiere.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projet_IMA
+{
+    class Lumiere
+    {
+        Couleur Couleur;
+        readonly float intensite;
+        V3 position;
+        V3 direction;
+
+        public Lumiere(Couleur couleur, float intensite, V3 position, V3 direction)
+        {
+            Couleur = couleur;
+            this.intensite = intensite;
+            this.position = position;
+            this.direction = direction;
+        }
+    }
+}
