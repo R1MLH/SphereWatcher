4e0545783ecf37b0326a804aaf41b4361ea6c9f5
Erwan <erwan.mailharro@edu.esiee.fr>
2019-10-15 14:23:57 +0200

    🕯💡🔦 NOUVELLES LAMPES 🔦💡🕯

--- Projet_IMA/Lumiere.cs
+++ Projet_IMA/Lumiere.cs
@@ -5,20 +5,54 @@ using System.Text;
 
 namespace Projet_IMA
 {
-    class Lumiere
+    abstract class Lumiere
     {
-        Couleur Couleur;
-        readonly float intensite;
-        V3 direction;
+        protected Couleur Couleur;
+        protected float intensite;
 
-        public Lumiere(Couleur couleur, float intensite, V3 direction)
+        public Lumiere(Couleur couleur, float intensite)
         {
             Couleur = couleur;
             this.intensite = intensite;
-            this.direction = direction;
         }
 
         public Couleur GetCouleur() { return Couleur*intensite; }
-        public V3 GetDirection() { return direction; }
+        public abstract V3 GetDirection(V3 point);
     }
+
+    class LampeDirectionelle : Lumiere
+    {
+        protected V3 direction;
+
+        public LampeDirectionelle(Couleur couleur, float intensite,V3 direction) : base(couleur,intensite)
+        {
+            this.direction = direction;
+        }
+
+        public override V3 GetDirection(V3 point)
+        {
+            return this.direction;
+        }
+    }
+
+    class LampePonctuelle : Lumiere
+    {
+        protected V3 position;
+        protected float decay;
+
+        public LampePonctuelle(Couleur couleur, float intensite,V3 position, float decay) : base(couleur, intensite)
+        {
+            this.position = position;
+            this.decay = decay;
+        }
+
+        public override V3 GetDirection(V3 point)
+        {
+            V3 sens = point - this.position;
+            sens.Normalize();
+            return sens;
+        }
+    }
+
+
 }
