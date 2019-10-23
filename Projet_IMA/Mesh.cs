c40633a31e2cccc96c7f2e5b6de1b2b311c8a186
Erwan <erwan.mailharro@edu.esiee.fr>
2019-10-23 18:39:23 +0200

    mesh loader fini

--- Projet_IMA/Mesh.cs
+++ Projet_IMA/Mesh.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Globalization;
 
 namespace Projet_IMA
 {
@@ -23,12 +24,29 @@ namespace Projet_IMA
             {
                 string[] items = line.Split(' ');
                 if (items[0] == "#" || items[0] == "") continue;
-                if (items[0] == "v") vertices.Add(new V3(float.Parse(items[1]), float.Parse(items[2]), float.Parse(items[3])));
-                if (items[0] == "f") Polygons.Add(new Triangle(new Couleur(0.5f, 0.5f, 0.5f), "n", vertices[int.Parse(items[1])], vertices[int.Parse(items[2])], vertices[int.Parse(items[3])]));
-
+                if (items[0] == "v") {
+                    float a = float.Parse(items[1], CultureInfo.InvariantCulture);
+                    float b = float.Parse(items[2], CultureInfo.InvariantCulture);
+                    float c = float.Parse(items[3], CultureInfo.InvariantCulture);
+                    vertices.Add(new V3(a,b,c));
+                }
+                if (items[0] == "f")
+                {
+                    string[] item1 = items[1].Split('/');
+                    string[] item2 = items[2].Split('/');
+                    string[] item3 = items[3].Split('/');
+                    Polygons.Add(new Triangle(new Couleur(0.5f, 0.5f, 0.5f), "n", vertices[(int.Parse(item1[0]))-1], vertices[(int.Parse(item2[0]))-1], vertices[(int.Parse(item3[0])-1)]));
+                }
             }
         }
 
-
+        public void Rescale(float factor)
+        {
+            foreach(Triangle t in Polygons)
+            {
+                t.Rescale(factor);
+            }
+        }
+        public List<Triangle> GetPolygons() { return Polygons; }
     }
 }
